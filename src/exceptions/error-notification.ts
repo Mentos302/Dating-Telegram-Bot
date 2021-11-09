10e4ce650665442ba25567780294a79f11b0b657
Ihor Melnyk <73445190+Mentos302@users.noreply.github.com>
2021-11-09 02:13:08 +0200

    fix error notf

--- src/exceptions/error-notification.ts
+++ src/exceptions/error-notification.ts
@@ -2,6 +2,8 @@ import { Extra } from 'telegraf'
 import { TelegrafContext } from 'telegraf/typings/context'
 
 export default (bot: TelegrafContext, error: Error) => {
+  console.log(error)
+
   const errorMsg: string = JSON.stringify(error)
 
   bot.telegram.sendMessage(
